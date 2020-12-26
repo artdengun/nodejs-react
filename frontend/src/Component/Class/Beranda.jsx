3538f033bfdca97240cd4a9ed13a2c3c13326eba
artdengun <artdengun@gmail.com>
2020-12-26 14:37:57 +0700

    props and state

---
+++ frontend/src/Component/Class/Beranda.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+
+
+class Beranda extends Component{
+    render(){
+        return(
+            <div>
+                <p>Hello world statefull</p>
+            </div>
+        );
+    }
+}
+
+export default Beranda;
---
+++ frontend/src/Component/Fungsional/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Blog from './Blog';
+import Produx from '../Class/Produx'
+
+const Home = () => {
+    return (
+        <div>
+            { <Produx nama="Macbook Air 2020" stock="10" harga="3300000" />/* <Blog
+                tanggal="05 juli 2020"
+                judul="Belajar Node Js"
+                summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. " />
+
+            <Blog
+                tanggal="10 juli 2020"
+                judul="Belajar React Js"
+                summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. " />
+
+
+            <Blog
+                tanggal="30 juli 2020"
+                judul="Belajar Komputerisasi Akutansi"
+                summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. " />
+
+ */}
+
+        </div>
+    );
+}
+export default Home;
